82f4ead204e36cd1475a24272b3ee19b47766efc
Harika Suvak <hcodes11@gmail.com>
2022-01-08 13:15:31 -0600

    done

--- src/views/SignIn.js
+++ src/views/SignIn.js
@@ -1,22 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
 import { Spinner } from 'reactstrap';
 import { signInUser } from '../api/auth';
-import image from '../images/movie2.jpg';
-
-const Background = styled.div`
-  display: flex;
-  background-image: url(${image});
-  flex-wrap: wrap;
-  position: absolute;
-  padding: 20px;
-  align-items: center;
-  height: 100%;
-  width: 100%;
-  object-fit: fill;
-  background-repeat: no-repeat;
-`;
 
 export default function SignIn({ user }) {
   return (
@@ -29,16 +14,23 @@ export default function SignIn({ user }) {
           />
         </div>
       ) : (
-        <Background>
-          <h1>Welcome to Film Frugal!</h1>
-          <button
-            type="button"
-            className="btn btn-success"
-            onClick={signInUser}
-          >
-            Sign In
-          </button>
-        </Background>
+        <>
+          <div>
+            <h1>Welcome to Film Frugal!</h1>
+          </div>
+          <div>
+            <h4>Developed by Harika</h4>
+          </div>
+          <div>
+            <button
+              type="button"
+              className="btn btn-success"
+              onClick={signInUser}
+            >
+              Sign In to Search Movies!
+            </button>
+          </div>
+        </>
       )}
     </>
   );
